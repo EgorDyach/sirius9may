30fd6eb2efcf20b92737e2d0ac90ca7f55acb54e
EgorDyach <tvoysosed33@gmail.com>
2024-04-13 14:52:18 +0300

    micro fixing

--- src/shared/AdminAllRequests/AdminAllRequests.tsx
+++ src/shared/AdminAllRequests/AdminAllRequests.tsx
@@ -1,24 +1,24 @@
 import './adminallrequests.css';
-const NO_UNREADED_PERSON = {
-  name: '',
-  city: "",
-  dateOfBirth: 0,
-  dateOfDeath: 0,
-  history: '',
-  mainPhoto: '',
-  medals: [],
-  photos: [],
-  published: 0,
-  rank: '',
-  contacts: {
-    telegram: "",
-    email: "",
-    name: "",
-    surname: "",
-    lastName: ""
-  },
-  id: ''
-}
+// const NO_UNREADED_PERSON = {
+//   name: '',
+//   city: "",
+//   dateOfBirth: 0,
+//   dateOfDeath: 0,
+//   history: '',
+//   mainPhoto: '',
+//   medals: [],
+//   photos: [],
+//   published: 0,
+//   rank: '',
+//   contacts: {
+//     telegram: "",
+//     email: "",
+//     name: "",
+//     surname: "",
+//     lastName: ""
+//   },
+//   id: ''
+// }
 
 export function AdminAllRequsts() {
     return '';
