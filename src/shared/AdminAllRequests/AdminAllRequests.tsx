0d6877b88faf83592942e4b32dfc3d489a0dd917
EgorDyach <tvoysosed33@gmail.com>
2024-04-25 03:29:50 +0300

    added admin all requests block

--- src/shared/AdminAllRequests/AdminAllRequests.tsx
+++ src/shared/AdminAllRequests/AdminAllRequests.tsx
@@ -1,4 +1,12 @@
+import { collection, query, limit, where, getDocs } from 'firebase/firestore';
+import { useState, useEffect } from 'react';
+import { db } from '../../firebase';
+import { useAppDispatch, useAppSelector } from '../../hooks/reduxHooks';
+import { removeNewPersons, addNewPerson } from '../../store/newPersons';
+import { PersonType, removePersons, addPerson } from '../../store/personsSlice';
 import './adminallrequests.css';
+import { Text } from '../../components/Text';
+import { AdminAllCard } from './AdminAllCard';
 // const NO_UNREADED_PERSON = {
 //   name: '',
 //   city: "",
@@ -21,5 +29,60 @@ import './adminallrequests.css';
 // }
 
 export function AdminAllRequsts() {
-    return '';
+    const THREE_DAYS = 259_200;
+
+    const [isPersonsLoading, setIsPersonsLoading] = useState(true);
+    const dispatch = useAppDispatch();
+    const newPersons = useAppSelector(state => state.newPersons.persons);
+    const persons = useAppSelector(state => state.persons.persons);
+    const [sizeOfNew, setSizeOfNew] = useState(0);
+    const [activeIndex, setActiveIndex] = useState(0);
+    const [historiesArrays, setHistoriesArrays] = useState<PersonType[][]>([[]]);
+    const [offset, setOffset] = useState(6);
+
+    useEffect(() => {
+        const getPersons = async () => {
+            const docRef = collection(db, "persons");
+            const q = query(docRef, limit(15), where("published", ">=", new Date().getTime() / 1000 - THREE_DAYS));
+            const querySnapshot = await getDocs(q);
+            setSizeOfNew(querySnapshot.size);
+            dispatch(removeNewPersons())
+            querySnapshot.forEach((doc) => {
+                const qq = doc.data();
+                dispatch(addNewPerson({ ...qq, id: doc.id }));
+            });
+
+            setIsPersonsLoading(false)
+        }
+
+        const getNewPersons = async () => {
+            const docRef = collection(db, "persons");
+            const qNotNew = query(docRef, limit(6 - sizeOfNew % 6), where("published", "<", new Date().getTime() / 1000 - THREE_DAYS));
+            await getDocs(qNotNew).then(querySnapshotNotNew => {
+                dispatch(removePersons())
+                querySnapshotNotNew.forEach((doc) => {
+                    const qq = doc.data();
+                    dispatch(addPerson({ ...qq, id: doc.id }));
+                })
+            });
+        }
+
+        getNewPersons();
+        getPersons();
+    }, [])
+    return <div className='adminAllRequests'>
+        {isPersonsLoading && <div>
+            Загрузка...
+        </div>}
+        {!isPersonsLoading && [...newPersons, ...persons].length !== 0 && <>
+            <div className="adminAllRequests__wrapper">
+                {[...newPersons, ...persons].sort().splice(0, offset).map((e) => {
+                    return <AdminAllCard e={e} />
+                })}
+            </div>
+            <button disabled={offset >= [...newPersons, ...persons].length} onClick={() => setOffset(offset + 6)} className="adminAllRequests__button">
+                <Text color='#fff' size={24}>Показать еще</Text>
+            </button>
+        </>}
+    </div>
 }
